cc867682449e39454c53f6f6350e0afa101bb263
Wryth <lucasbriatte@outlook.fr>
2022-01-19 00:02:31 +0100

    Ajout du router sur le home

--- pages/home.tsx
+++ pages/home.tsx
@@ -18,129 +18,10 @@ dayjs.locale("fr");
 
 type Props = {
   session: Session | null,
-  notes?: HomeNote,
-  years?: number[],
-  currentYear?: number;
-}
-
-type State = {
-  year: null | number,
-  month: number,
-}
-
-enum DataState {NONE, SENT_AND_WAITING, ERROR, VALID};
-type Data = {label: string; state: DataState, index: number}
-
-function getSegmentedData(props: Props, state: State): Data[] {
-  const yearlyNote = state.year ? ((props?.notes ?? []).find(note => note.annee === state.year) ?? null) : null;
-  return dayjs.months().map((month, monthIndex) => {
-    const monthNote: INoteDeFrais | null = yearlyNote ? (yearlyNote.notes.find(note => note.mois === monthIndex) ?? null) : null;
-
-    return {
-      label: `${month}`,
-      state: !monthNote ? DataState.NONE : 
-        monthNote.etat === NOTEDEFRAIS_ETAT.EN_ATTENTE_DE_VALIDATION ? DataState.SENT_AND_WAITING : 
-        monthNote.etat === NOTEDEFRAIS_ETAT.REFUSEE ? DataState.ERROR :
-        monthNote.etat === NOTEDEFRAIS_ETAT.VALIDEE ? DataState.VALID : DataState.NONE,
-      index: monthIndex
-    }
-  })
 }
 
 export default function Home(props: Props) {
-    const [state, setState] = useState({
-      year: props?.currentYear as null | number,
-      month: 0
-    });
-
-    const currentNote = props.notes?.find(note => note.annee === state.year)?.notes?.find(note => note.mois === state.month) ?? null;
-
-    const renderNote = (note: INoteDeFrais | null) => {
-      const rows = (note?.ligne ?? []).map((ligne, index) => (
-        <tr key={index}>
-          <td>{ligne.titre}</td>
-          <td>{dayjs(ligne.date).format("DD-MM-YYYY")}</td>
-          <td>{numbro(ligne.prixHT).formatCurrency({ mantissa: 2, 
-            currencySymbol: "€", 
-            currencyPosition: "postfix",
-            spaceSeparated: true ,
-            spaceSeparatedCurrency: true,
-            thousandSeparated: true,
-          }).replace(",", " ")}</td>
-          <td>{"TODO"}</td>
-        </tr>
-      ));
-
-      return <Table striped highlightOnHover>
-        <thead>
-          <tr>
-            <th>Titre</th>
-            <th>Date</th>
-            <th>Montant HT</th>
-            <th>Justificatif</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </Table>
-    }
-
-    return <Group grow direction="column" style={{width: "100%"}} spacing={0}>
-      <Group style={{alignItems: "baseline"}} grow>
-        <Container padding="lg">
-          {renderNote(currentNote)}
-        </Container>
-      </Group>
-      <Group style={{flex: 0, width: "100%"}} spacing={0}>
-        <Select
-          placeholder="Année"
-          data={(props?.years ?? []).map(year => { return {
-              value: `${year}`,
-              label: `${year}`
-            }
-          })}
-          value={state.year ? `${state.year}` : null}
-          onChange={(item: string) => {
-            setState({
-              ...state,
-              year: parseInt(item)
-            });
-          }}
-          style={{
-            flex: "unset"
-          }}
-        />
-        <SegmentedControl style={{flex: 1}} value={`${state.month}`} onChange={(item: string) => {
-          setState({
-            ...state,
-            month: parseInt(item)
-          });
-          }} fullWidth data={getSegmentedData(props, state).map(el => {
-          var icon = <></>;
-          switch (el.state) {
-            case DataState.NONE:
-              break;
-            case DataState.SENT_AND_WAITING:
-              icon = <HiClock />
-              break;
-            case DataState.ERROR:
-              icon = <HiXCircle />
-              break;
-            case DataState.VALID:
-              icon = <HiCheck />
-              break;
-          }
-          return {
-            value: `${el.index}`,
-            label: (
-              <Center>
-                <div style={{ marginRight: 10, textTransform: "capitalize" }}>{el.label}</div>
-                {icon}
-              </Center>
-            ),
-          }
-        })} />
-      </Group> 
-    </Group>
+  return <p>Loading</p>
 }
 
 export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
@@ -156,15 +37,12 @@ export const getServerSideProps: GetServerSideProps<Props> = async (context) => 
   }
 
   const currentYear = dayjs().year();
-  const notes = JSON.parse(JSON.stringify(await getHomeNote(session))) as HomeNote;
-  const years = notes.map(note => note.annee);
 
   return {
-    props: {
-      session,
-      notes,
-      years,
-      currentYear
+    redirect: {
+      permanent: false,
+      destination: `/home/${currentYear}`,
     },
+    props: { session }
   }
 }
---
+++ pages/home/[params].tsx
@@ -0,0 +1,185 @@
+import { Group, SegmentedControl, Center, Select, Table, GroupedTransition, Container, Loader } from '@mantine/core'
+import type { GetServerSideProps } from 'next'
+import { Session } from 'next-auth'
+import { getSession } from 'next-auth/react'
+import { getHomeNote, HomeNote } from '../api/home'
+import { useEffect, useState } from 'react'
+import { HiClock, HiXCircle, HiCheck } from "react-icons/hi";
+import dayjs from 'dayjs'
+import "dayjs/locale/fr";
+import localeData from "dayjs/plugin/localeData";
+import { INoteDeFrais } from '../../entity/notedefrais.entity'
+import { NOTEDEFRAIS_ETAT } from '../../entity/utils'
+import numbro from 'numbro'
+import { useRouter } from 'next/router'
+dayjs.extend(localeData);
+dayjs().format();
+dayjs.locale("fr");
+
+type Props = {
+  session: Session | null,
+  notes?: INoteDeFrais[],
+  years?: number[],
+  currentYear?: number;
+}
+
+type State = {
+  note: INoteDeFrais | null,
+  month: number,
+}
+
+enum DataState {NONE, SENT_AND_WAITING, ERROR, VALID};
+type Data = {label: string; state: DataState, index: number}
+
+function getSegmentedData(props: Props): Data[] {
+  return dayjs.months().map((month, monthIndex) => {
+    const monthNote: INoteDeFrais | null = props.notes ? (props.notes.find(note => note.mois === monthIndex) ?? null) : null;
+
+    return {
+      label: `${month}`,
+      state: !monthNote ? DataState.NONE : 
+        monthNote.etat === NOTEDEFRAIS_ETAT.EN_ATTENTE_DE_VALIDATION ? DataState.SENT_AND_WAITING : 
+        monthNote.etat === NOTEDEFRAIS_ETAT.REFUSEE ? DataState.ERROR :
+        monthNote.etat === NOTEDEFRAIS_ETAT.VALIDEE ? DataState.VALID : DataState.NONE,
+      index: monthIndex
+    }
+  })
+}
+
+export default function Home(props: Props) {
+  const router = useRouter();
+  const year = parseInt(router.query.params as string);
+  const [state, setState] = useState({
+    note: null,
+    month: 0
+  });
+
+  useEffect(() => {
+    (async () => {
+      // const request = await fetch(`/api/${props?.notes?.find(note => note.mois === state.month)?.id}`);
+      // const result = await request.json();
+
+      // setState({
+      //   ...state,
+      //   note: result
+      // });
+    })()
+  }, []);
+
+  const renderNote = () => {
+    if (!state.note) {
+      return <Center style={{height: "100%"}}>
+        <Loader />
+      </Center>
+    }
+
+    const {note}: State = state;
+    const rows = (note?.ligne ?? []).map((ligne, index) => (
+      <tr key={index}>
+        <td>{ligne.titre}</td>
+        <td>{dayjs(ligne.date).format("DD-MM-YYYY")}</td>
+        <td>{numbro(ligne.prixHT).formatCurrency({ mantissa: 2, 
+          currencySymbol: "€", 
+          currencyPosition: "postfix",
+          spaceSeparated: true ,
+          spaceSeparatedCurrency: true,
+          thousandSeparated: true,
+        }).replace(",", " ")}</td>
+        <td>{"TODO"}</td>
+      </tr>
+    ));
+
+    return <Container padding="lg">
+      <Table striped highlightOnHover>
+        <thead>
+          <tr>
+            <th>Titre</th>
+            <th>Date</th>
+            <th>Montant HT</th>
+            <th>Justificatif</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+    </Container>
+  }
+
+  return <Group grow direction="column" style={{width: "100%"}} spacing={0}>
+    <Group style={{alignItems: "baseline"}} grow>
+      {renderNote()}
+    </Group>
+    <Group style={{flex: 0, width: "100%"}} spacing={0}>
+      <Select
+        placeholder="Année"
+        data={(props?.years ?? []).map(year => { return {
+            value: `${year}`,
+            label: `${year}`
+          }
+        })}
+        value={year ? `${year}` : null}
+        onChange={(item: string) => {
+          router.push(`/home/${item}`);
+        }}
+        style={{
+          flex: "unset"
+        }}
+      />
+      <SegmentedControl style={{flex: 1}} value={`${state.month}`} onChange={(item: string) => {
+        setState({
+          ...state,
+          month: parseInt(item)
+        });
+        }} fullWidth data={getSegmentedData(props).map(el => {
+        var icon = <></>;
+        switch (el.state) {
+          case DataState.NONE:
+            break;
+          case DataState.SENT_AND_WAITING:
+            icon = <HiClock />
+            break;
+          case DataState.ERROR:
+            icon = <HiXCircle />
+            break;
+          case DataState.VALID:
+            icon = <HiCheck />
+            break;
+        }
+        return {
+          value: `${el.index}`,
+          label: (
+            <Center>
+              <div style={{ marginRight: 10, textTransform: "capitalize" }}>{el.label}</div>
+              {icon}
+            </Center>
+          ),
+        }
+      })} />
+    </Group> 
+  </Group>
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+      props: {session}
+    }
+  }
+
+  const currentYear = parseInt(context.query.params as string);
+  const notes = JSON.parse(JSON.stringify(await getHomeNote(session))) as HomeNote;
+  const years = notes.map(note => note.annee);
+
+  return {
+    props: {
+      session,
+      notes: (notes.find(note => note.annee === currentYear))?.notes,
+      years,
+      currentYear
+    },
+  }
+}
