5bf5cd9943595cacfb87fbce450dc9b2693d6dcd
Vianney Demarquet <vianney.demarquet@gmail.com>
2022-02-09 15:14:15 +0100

    indent + supression log

--- pages/api/[note].ts
+++ pages/api/[note].ts
@@ -6,9 +6,9 @@ import { INoteDeFrais, NoteDeFrais, noteToApi } from "../../entity/notedefrais.e
 import { NOTEDEFRAIS_ETAT } from "../../entity/utils";
 import { prepareConnection } from "./database";
 
-export type NotesRequest = INoteDeFrais | RequestError | {message: string};
+export type NotesRequest = INoteDeFrais | RequestError | { message: string };
 
-export async function getNote(noteId: string, userId: string): Promise<NotesRequest | null>{
+export async function getNote(noteId: string, userId: string): Promise<NotesRequest | null> {
 
     await prepareConnection();
     const conn = getConnection();
@@ -16,8 +16,8 @@ export async function getNote(noteId: string, userId: string): Promise<NotesRequ
         .createQueryBuilder("notedefrais")
         .leftJoinAndSelect("notedefrais.lignes", "lignedefrais")
         .leftJoinAndSelect("lignedefrais.mission", "mission")
-        .where("notedefrais.id = :id", {id: noteId})
-        .andWhere("userId = :user", {user:userId})
+        .where("notedefrais.id = :id", { id: noteId })
+        .andWhere("userId = :user", { user: userId })
         .getOne();
     conn.close();
 
@@ -29,47 +29,47 @@ export async function getNote(noteId: string, userId: string): Promise<NotesRequ
 
 }
 
-export async function rmNote(noteId: string, userId: string): Promise<boolean>{
+export async function rmNote(noteId: string, userId: string): Promise<boolean> {
     await prepareConnection();
     var conn = getConnection();
-  
+
     const note = await conn.getRepository(NoteDeFrais)
-      .createQueryBuilder("notedefrais")
-      .where("notedefrais.id = :id", {id: noteId})
-      .andWhere("userId = :user", {user:userId})
-      .getOne();
-  
+        .createQueryBuilder("notedefrais")
+        .where("notedefrais.id = :id", { id: noteId })
+        .andWhere("userId = :user", { user: userId })
+        .getOne();
+
     conn.close();
-  
+
     if (!note) {
-      throw new Error("note inexistante");
-    }else if (!(note.etat === NOTEDEFRAIS_ETAT.BROUILLON || note.etat === NOTEDEFRAIS_ETAT.REFUSEE)) {
-      return false;
+        throw new Error("note inexistante");
+    } else if (!(note.etat === NOTEDEFRAIS_ETAT.BROUILLON || note.etat === NOTEDEFRAIS_ETAT.REFUSEE)) {
+        return false;
     }
     await prepareConnection();
-  conn = getConnection();
-  
-  await conn.createQueryBuilder()
-  .delete()
-  .from(NoteDeFrais)
-  .where("id = :id",{id: noteId})
-  .execute();
-    
-  return true;
+    conn = getConnection();
+
+    await conn.createQueryBuilder()
+        .delete()
+        .from(NoteDeFrais)
+        .where("id = :id", { id: noteId })
+        .execute();
+
+    return true;
 }
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<NotesRequest>
-  ) {
+) {
     var userId: string | null = null;
     try {
-        
+
         //recupération de la session
         const session = await getSession({ req });
         if (session) {
             userId = (session as any)?.id;
         } else {
-            res.status(403).json({error: "acces interdit" as string, code: 403});
+            res.status(403).json({ error: "acces interdit" as string, code: 403 });
         }
 
         switch (req.method) {
@@ -77,30 +77,29 @@ export default async function handler(
                 const note = await getNote(req.query?.note as string, userId as string)
 
                 if (!note) {
-                throw Error;
+                    throw Error;
                 }
                 res.status(200).json(note);
                 break;
             case "DELETE":
                 const rm = await rmNote(req.query?.note as string, userId as string);
-                if(rm){
-                    res.status(200).json({message: "note supprimé"});
-                }else{
-                    res.status(423).json({error: "Vous ne pouvez pas supprimer cette note" as string, code: 423});
+                if (rm) {
+                    res.status(200).json({ message: "note supprimé" });
+                } else {
+                    res.status(423).json({ error: "Vous ne pouvez pas supprimer cette note" as string, code: 423 });
                 }
                 break;
-                    
+
             default:
-                res.status(424).json({error : "methode non prise en charge" as string, code : 424})
+                res.status(424).json({ error: "methode non prise en charge" as string, code: 424 })
                 break;
         }
 
-  
-        
-        
-    } catch(e) {
-        console.log(e);
-        res.status(404).json({error: e as string, code: 404});
+
+
+
+    } catch (e) {
+        res.status(404).json({ error: e as string, code: 404 });
     }
-    
-  }
+
+}
