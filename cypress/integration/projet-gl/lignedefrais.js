d9a4287eb7bcf244b4198a3907b0fcb292e15127
TMWilliam <tri-man-william.vu@u-psud.fr>
2022-02-11 00:05:04 +0100

    Scenario 1 done, Scenario 2 in progress

--- cypress/integration/projet-gl/lignedefrais.js
+++ cypress/integration/projet-gl/lignedefrais.js
@@ -12,11 +12,11 @@ describe('Tests sur les lignes de frais', () => {
     //cy.wait(2000)
     cy.get('input[name=email]').type('user')
     cy.get('input[name=password]').type('user')
-    //cy.wait(2000)
+    //cy.wait(1000)
     cy.contains('Se connecter').click()    
   })
 
-  it('Scénario 1 - Création d\'une ligne de frais validable', () => {
+  it('Scénario 1 - Création d\'une ligne de frais', () => {
     cy.get('button').first().click()
     cy.get('[placeholder="Donnez un titre à cette ligne de frais"]').type('restaurant')
     cy.get('[placeholder="Sélectionnez la date"]').click()
@@ -26,27 +26,46 @@ describe('Tests sur les lignes de frais', () => {
     cy.contains('REPAS').click()
     cy.get('[placeholder="Sélectionnez la mission associée"]').click()
     cy.contains('Davis - Walker').click()
-    cy.contains('Montant HT').parent().children().should('have.length', 2).last().clear().type('30')
-    cy.contains('Montant TVA').parent().children().should('have.length', 2).last().clear().type('3')
+    cy.contains('Montant HT').parent().children().should('have.length', 2).last().clear().type('38.82')
+    cy.contains('Montant TVA').parent().children().should('have.length', 2).last().clear().type('3.88')
     cy.contains('Faites glisser').parent().parent().parent().click()
-
-    // const filepath = 'images/evening.png'
+    const filepath = 'restau1.jpg'
     cy.get('input[type="file"]').attachFile(filepath)
-    // cy.get('#file-submit').click()
-    // cy.get('#uploaded-files').contains('evening.png')
-
-    // .within(() => {
-    //   // all searches are automatically rooted to the found tr element
-    //   cy.get('td').eq(1).contains('My first project')
-    //   cy.get('td').eq(2).contains('0')
-    //   cy.get('td').eq(3).contains('Active')
-    //   cy.get('td').eq(4).contains('button', 'Edit').click()
-    // })
-
-    // cy.get('input').eq(2).click({force:true})
-    // cy.get('button').last().should('be.disabled')
+    cy.get('textarea').type('Dîner professionnel avec le client')
+    cy.get('button').last().click()
+    cy.wait(3000)
   })
 
+  it('Scénario 2 - Modification d\'une ligne de frais', () => {
+    cy.get('h3').first().click()
+    cy.get('tbody').first().find('td').last().find('button').first().click()
+    // cy.get('[placeholder="Donnez un titre à cette ligne de frais"]').type('restaurant')
+    // cy.get('[placeholder="Sélectionnez la date"]').click()
+    // cy.get('table').find('td').eq(15).click()
+    // cy.wait(1000)
+    // cy.get('[placeholder="Sélectionnez le type de frais"]').click()
+    // cy.contains('REPAS').click()
+    // cy.get('[placeholder="Sélectionnez la mission associée"]').click()
+    // cy.contains('Davis - Walker').click()
+    // cy.contains('Montant HT').parent().children().should('have.length', 2).last().clear().type('30')
+    // cy.contains('Montant TVA').parent().children().should('have.length', 2).last().clear().type('3')
+    // cy.contains('Faites glisser').parent().parent().parent().click()
+    // const filepath = 'restau1.jpg'
+    // cy.get('input[type="file"]').attachFile(filepath)
+    // cy.get('textarea').type('Dîner professionnel avec le client')
+    // cy.get('button').last().click()
+  })
+
+  // .within(() => {
+  //   // all searches are automatically rooted to the found tr element
+  //   cy.get('td').eq(1).contains('My first project')
+  //   cy.get('td').eq(2).contains('0')
+  //   cy.get('td').eq(3).contains('Active')
+  //   cy.get('td').eq(4).contains('button', 'Edit').click()
+  // })
+
+  // cy.get('input').eq(2).click({force:true})
+  // cy.get('button').last().should('be.disabled')
 
   // cy.wrap({ foo: 'bar' }).its('foo').should('eq', 'bar')
 
