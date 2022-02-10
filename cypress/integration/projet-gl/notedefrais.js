26281c5336cf0563760d244c27430ae336ce7774
TMWilliam <tri-man-william.vu@u-psud.fr>
2022-02-10 18:21:11 +0100

    Basic test

--- cypress/integration/projet-gl/notedefrais.js
+++ cypress/integration/projet-gl/notedefrais.js
@@ -25,26 +25,11 @@ describe('Affichage des notes de frais', () => {
     cy.contains('2021').click()
     cy.wait(5000)
 
-    cy.get('input').first().click()
-    cy.wait(3000)
-    cy.contains('2018').click()
-    cy.wait(5000)
-
-    // ICI pour l'affichage, les cookies bugs dans cypress ce qui nous ramene a la page d'accueil
-    // de la route/home mais dont il faut alors se connecter à nouveau
     cy.get('input').eq(2).click({force:true})
     cy.get('button').last().should('be.disabled')
     cy.get('input').eq(5).click({force:true})
     cy.get('button').last().should('be.disabled')
   })
-
-  // Gros problèmes au niveau de cette branche :
-  // - Dans le cas où dans un mois il y a deja au moins une note de frais,
-  //  le bouton "sauvegarder" sauvegarde quoi ? car dans un mois où il y a 2 notes de frais,
-  //  il n'y a aucune selection de quelle note de frais est à sauvegarder. De même pour le bouton "supprimer".
-  // - Dans le cas où il n'y a aucune note pour un mois donné,
-  //  le bouton "sauvegarder" est enable. Pourquoi ? Et comment créer une note de frais ?
-  // - Plein de cas comme ça encore...
 
   // tester un .children() si cela existe
   // puis .children().eq(2)
